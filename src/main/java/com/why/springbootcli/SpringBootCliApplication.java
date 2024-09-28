37e5900e66616e1037d11e9f98054f9a5fad04b0
weihongyu1 <488009667@qq.com>
2024-09-28 14:55:27 +0800

    初步搭建spring boot项目

---
+++ src/main/java/com/why/springbootcli/SpringBootCliApplication.java
@@ -0,0 +1,13 @@
+package com.why.springbootcli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCliApplication.class, args);
+	}
+
+}
